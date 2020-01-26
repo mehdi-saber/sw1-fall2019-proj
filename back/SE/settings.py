bee935a67a4871a2b1dc59c4f139b83205e9becc
9525038 <9525038@ceit.local>
2020-01-26 18:12:06 +0330

    added allow any to port connetions

--- back/SE/settings.py
+++ back/SE/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'ra%a6j2)&fzv=@p0cnw9h0t+=6a))kb18)schjwb*kzsm_afy)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
