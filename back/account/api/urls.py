93d0683477bf0d2d406e370efc8e517efb840d7c
mohamad sabermahani <sabermahani.mohamad@gmail.com>
2020-01-28 01:43:15 +0330

    update

--- back/account/api/urls.py
+++ back/account/api/urls.py
@@ -8,23 +8,23 @@ app_name = 'account'
 urlpatterns = [
     # Sign up
     path('register', api_register_user, name="user_register"),
-    path('register/verify/', api_register_student_verification, name="student_verification"),
-    path('register/verification/refresh_code/', api_refresh_student_verification_code, name="verification_refresh"),
+    path('register/verify', api_register_student_verification, name="student_verification"),
+    path('register/verification/refresh_code', api_refresh_student_verification_code, name="verification_refresh"),
 
 
     # Sign in
-    path('token/', TokenObtainPairView.as_view()),
-    path('token/refresh/', TokenRefreshView.as_view()),
+    path('token', TokenObtainPairView.as_view()),
+    path('token/refresh', TokenRefreshView.as_view()),
 
 
     # Reset password
-    path('reset/', api_reset_password, name="reset_password"),
-    path('apply-password/', api_apply_password, name="reset_password"),
+    path('reset', api_reset_password, name="reset_password"),
+    path('apply-password', api_apply_password, name="reset_password"),
 
     # Edit account
-    path('edit/', edit_account, name="edit"),
+    path('edit', edit_account, name="edit"),
 
-    path('info/', get_user_tweets, name="get_user_tweets"),
+    path('info', get_user_tweets, name="get_user_tweets"),
 
 ]
 
