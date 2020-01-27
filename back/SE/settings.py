d51ac7c9b4d066a6daf2c3b9a7b087f7f3d2ee0f
mohamad sabermahani <sabermahani.mohamad@gmail.com>
2020-01-28 00:53:04 +0330

    update

--- back/SE/settings.py
+++ back/SE/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'account',
-    'corsheaders'
+    'corsheaders',
     'tweet',
 ]
 
@@ -55,7 +55,6 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 ]
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
